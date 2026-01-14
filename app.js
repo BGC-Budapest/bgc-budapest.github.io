3d4a613d441861b5ee4deda196d2fab571ffb9f4
Geza <orgo.geza@gmail.com>
2026-01-14 15:28:43 +0100

    title adjustments

--- app.js
+++ app.js
@@ -67,8 +67,8 @@ const translations = {
         conversational: 'Társalgási',
         bluffing: 'Blöfföl',
         // Info box messages
-        englishGamesInfo: 'Az összes angol nyelvű játékunk a földszinten, a 37-38-as polcon található.',
-        twoPlayerGamesInfo: 'Az összes 2 fős játékunk az 1. emeleten, az 50-56-os polcon található.',
+        englishGamesInfo: 'Az összes angol nyelvű játékunk a földszinten, a 01-02 és 37-38-as polcon található.',
+        twoPlayerGamesInfo: 'Az összes dedikáltan 2 fős játékunk az 1. emeleten, az 50-56-os polcon található.',
         awardWinningInfo: 'Minden Spiel des Jahres díjnyertes játék az 1. emeleten, az 57-62-es polcon található.',
         resetButton: 'Újrakezdés'
     },
@@ -102,8 +102,8 @@ const translations = {
         conversational: 'Conversational',
         bluffing: 'Bluffing',
         // Info box messages
-        englishGamesInfo: 'All of our English games can be found on the ground floor on shelf 37-38.',
-        twoPlayerGamesInfo: 'All of our 2 player games can be found on the 1st floor on shelf 50-56.',
+        englishGamesInfo: 'All of our English games can be found on the ground floor on shelf 01-02 and 37-38.',
+        twoPlayerGamesInfo: 'All of our dedicated 2 player games can be found on the 1st floor on shelf 50-56.',
         awardWinningInfo: 'Every Spiel des Jahres award winning game can be found on the 1st floor on shelf 57-62.',
         resetButton: 'Reset'
     }
